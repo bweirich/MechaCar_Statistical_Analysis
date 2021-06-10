60305781d1fdb303f31b58024f7cef7591b65337
Rebecca Weirich <b.weirich@yahoo.com>
2021-06-09 22:11:18 -0500

    Add images and update script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,7 @@
 # import dplyr library
 library(dplyr)
 
+# Linear Regression MPG
 # import data and create database
 mecha_mpg <- read.csv('MechaCar_mpg.csv')
 
@@ -16,3 +17,20 @@ print(lm_mpg)
 
 # view summary of linear regression model for p-value
 summary(lm_mpg)
+
+# Summary Suspension Coils
+susp_coil <- read.csv('Suspension_Coil.csv')
+
+# view dataframe
+head(susp_coil)
+
+# create summary table
+total_summary <- summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),
+                           SD=sd(PSI), susp_coil)
+
+# groupby lots
+grp_lot <- susp_coil %>% group_by(Manufacturing_Lot)
+
+# create summary table for lots
+lot_summary <- summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),
+                         SD=sd(PSI), grp_lot)
--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This analysis ran a linear regression model on the car's factors including:
 
 to determine the affect on miles per gallon (MPG).
 
-The factors that have the most impact on MPG are Vehicle Length and Ground Clearance.  This is based on the PR(>|t|) value in the summary table below.
+The factors that have the most impact on MPG are Vehicle Length and Ground Clearance.  This is based on the PR(>|t|) value in the summary table above.
 
 The p-value of 5.35e-11 is well below the significance level of 0.05% whic means the slope is not zero.
 

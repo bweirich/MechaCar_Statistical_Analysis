007352e84e54783c2bad532a7ea4ce669b1fde5b
Rebecca Weirich <b.weirich@yahoo.com>
2021-06-09 21:02:04 -0500

    Add Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,15 @@
+# import dplyr library
+library(dplyr)
+
+# import data and create database
+mecha_mpg <- read.csv('MechaCar_mpg.csv')
+
+# view dataframe
+head(mecha_mpg)
+
+# create linear regression model
+lm_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,
+   data=mecha_mpg)
+
+# view summary of linear regression model for p-value
+summary(lm_mpg)
